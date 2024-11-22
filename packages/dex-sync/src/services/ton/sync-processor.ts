db34b4a977b37f23ccb8720ebe51bf92e3535fd6
perfogic <longdieu12x@gmail.com>
2024-11-22 15:45:15 +0700

    add sync processor

--- packages/dex-sync/src/services/ton/sync-processor.ts
+++ packages/dex-sync/src/services/ton/sync-processor.ts
@@ -1,22 +1,55 @@
-// import PoolRepository from "../../apis/repositories/pool.repository";
-// import PositionRepository from "src/apis/repositories/position.repository";
-// import { PoolWrapper,  } from "orbiton-contracts";
-// import { Address } from "@ton/core";
+import PoolRepository from "../../apis/repositories/pool.repository";
+import PositionRepository from "src/apis/repositories/position.repository";
+import { PoolWrapper } from "orbiton-contracts";
+import { Address } from "@ton/core";
+import {
+  Blockchain,
+  RemoteBlockchainStorage,
+  SandboxContract,
+  wrapTonClient4ForRemote,
+} from "@ton/sandbox";
+import { TonClient4 } from "@ton/ton";
+import { getHttpV4Endpoint } from "@orbs-network/ton-access";
+import { IPosition } from "../../@types";
 
-// export const syncPools = async () => {
-//   const pools = await PoolRepository.getAll();
-//   for (const pool of pools) {
-//     const poolContract = new PoolWrapper.PoolTest(
-//       Address.parse(pool.poolAddress)
-//     );
-//     const positions = await PositionRepository.getAll({
-//       poolId: pool._id.toString(),
-//     });
-//     const positionContracts = positions.map(
-//       (position) =>
-//         new PositionWr.PositionTest(Address.parse(position.positionAddress))
-//     );
-//   }
-// };
+export const syncPools = async () => {
+  let blockchain = await Blockchain.create({
+    storage: new RemoteBlockchainStorage(
+      wrapTonClient4ForRemote(
+        new TonClient4({
+          endpoint: await getHttpV4Endpoint({
+            network: "mainnet",
+          }),
+        })
+      )
+    ),
+  });
+  blockchain.verbosity = {
+    ...blockchain.verbosity,
+  };
+  const pools = await PoolRepository.getAll();
+  for (const pool of pools) {
+    const poolContract = blockchain.openContract(
+      new PoolWrapper.PoolTest(Address.parse(pool.poolAddress))
+    );
+    const positions = await PositionRepository.getAll({
+      poolId: pool._id.toString(),
+    });
+    await Promise.all(
+      positions.map((position) =>
+        syncPosition(
+          {
+            ...position,
+            poolId: position.poolId.toString(),
+          },
+          poolContract
+        )
+      )
+    );
+  }
+};
 
-// const syncPosition = async (positionAddress: string) => {};
+const syncPosition = async (
+  position: IPosition,
+  poolContract: SandboxContract<PoolWrapper.PoolTest>
+) => {};
