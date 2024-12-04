5b25ef844674a19e55de005294d77993b73b4b6d
perfogic <longdieu12x@gmail.com>
2024-12-05 02:42:43 +0700

    update code

--- packages/dex-sync/src/services/ton/tx-processor.ts
+++ packages/dex-sync/src/services/ton/tx-processor.ts
@@ -143,6 +143,7 @@ export default class TonTxProcessor {
           i++;
         }
       } catch (error) {
+        console.log(error);
         // reset latestProcessedTxHash so we can start over to prevent missed txs in case of having errors
         this.logger.error(
           "TonTxProcessor:Error querying unprocessed transactions: ",
@@ -192,9 +193,9 @@ export default class TonTxProcessor {
             ];
             this.sync(
               this.watchContracts,
-              Object.keys(
-                this.mapLatestProcessedHashOnContract[watchContract]
-              ).map((key) => this.mapLatestProcessedHashOnContract[key])
+              Object.keys(this.mapLatestProcessedHashOnContract).map(
+                (key) => this.mapLatestProcessedHashOnContract[key]
+              )
             );
             await PoolRepository.create({
               poolAddress: poolAddress.toString(),
