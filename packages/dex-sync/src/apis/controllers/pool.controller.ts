0e0939237c568f3225a0bc13683f80762ce744b7
perfogic <longdieu12x@gmail.com>
2024-12-05 02:23:39 +0700

    update code

--- packages/dex-sync/src/apis/controllers/pool.controller.ts
+++ packages/dex-sync/src/apis/controllers/pool.controller.ts
@@ -72,12 +72,7 @@ namespace PoolController {
         ),
       });
       let sender = blockchain.sender(Address.parse(senderAddress as string));
-      const [
-        allPoolsOne,
-        allPoolsTwo,
-        jettonWalletInContract,
-        jettonWalletOutContract,
-      ] = await Promise.all([
+      const [allPoolsOne, allPoolsTwo] = await Promise.all([
         PoolRepository.getAll({
           jetton0MasterAddress: jettonInAddress as string,
           jetton1MasterAddress: jettonOutAddress as string,
@@ -86,8 +81,6 @@ namespace PoolController {
           jetton0MasterAddress: jettonOutAddress as string,
           jetton1MasterAddress: jettonInAddress as string,
         }),
-        getJettonWalletClient(sender, jettonInAddress as string, blockchain),
-        getJettonWalletClient(sender, jettonOutAddress as string, blockchain),
       ]);
       const allPools = [...allPoolsOne, ...allPoolsTwo];
 
@@ -96,106 +89,104 @@ namespace PoolController {
       }
 
       // find best returned simulate
-      let returnedAmount = 0n;
+      // let returnedAmount = 0n;
       let data: any = null;
-      for (const pool of allPools) {
-        const zeroForOne =
-          jettonInAddress === pool.jetton0MasterAddress ? -1 : 0;
+      const pool = allPools[0];
+      const zeroForOne = jettonInAddress === pool.jetton0MasterAddress ? -1 : 0;
+      // for (const pool of allPools) {
+      //   const zeroForOne =
+      //     jettonInAddress === pool.jetton0MasterAddress ? -1 : 0;
 
-        const [beforeSenderExecute, beforeJettonReceived] = await Promise.all([
-          (async () => {
-            return (
-              await blockchain.getContract(
-                Address.parse(senderAddress as string)
-              )
-            ).balance;
-          })(),
-          (async () => {
-            return (await jettonWalletOutContract!.getBalance()).amount;
-          })(),
-        ]);
+      //   const [beforeSenderExecute, beforeJettonReceived] = await Promise.all([
+      //     (async () => {
+      //       return (
+      //         await blockchain.getContract(
+      //           Address.parse(senderAddress as string)
+      //         )
+      //       ).balance;
+      //     })(),
+      //     (async () => {
+      //       return (await jettonWalletOutContract!.getBalance()).amount;
+      //     })(),
+      //   ]);
 
-        console.log({
-          kind: "OpJettonTransferSwap",
-          query_id: 0,
-          jetton_amount: BigInt(jettonInAmount as string),
-          to_address: Address.parse(env.ton.router),
-          response_address: Address.parse(senderAddress as string),
-          custom_payload: beginCell().storeDict(Dictionary.empty()).endCell(),
-          forward_ton_amount: toNano(2.0),
-          either_payload: true,
-          swap: {
-            kind: "SwapParams",
-            forward_opcode: PoolWrapper.Opcodes.Swap,
-            fee: pool.fee,
-            jetton1_wallet: zeroForOne
-              ? Address.parse(pool.jetton1WalletAddress)
-              : Address.parse(pool.jetton0WalletAddress),
-            sqrt_price_limit: MIN_SQRT_RATIO,
-            tick_spacing: pool.tickSpacing,
-            zero_for_one: zeroForOne,
-          },
-        });
+      //   console.log({
+      //     kind: "OpJettonTransferSwap",
+      //     query_id: 0,
+      //     jetton_amount: BigInt(jettonInAmount as string),
+      //     to_address: Address.parse(env.ton.router),
+      //     response_address: Address.parse(senderAddress as string),
+      //     custom_payload: beginCell().storeDict(Dictionary.empty()).endCell(),
+      //     forward_ton_amount: toNano(2.0),
+      //     either_payload: true,
+      //     swap: {
+      //       kind: "SwapParams",
+      //       forward_opcode: PoolWrapper.Opcodes.Swap,
+      //       fee: pool.fee,
+      //       jetton1_wallet: zeroForOne
+      //         ? Address.parse(pool.jetton1WalletAddress)
+      //         : Address.parse(pool.jetton0WalletAddress),
+      //       sqrt_price_limit: MIN_SQRT_RATIO,
+      //       tick_spacing: pool.tickSpacing,
+      //       zero_for_one: zeroForOne,
+      //     },
+      //   });
 
-        let swapTx = await jettonWalletInContract!.sendTransferSwap(
-          sender,
-          {
-            kind: "OpJettonTransferSwap",
-            query_id: 0,
-            jetton_amount: BigInt(jettonInAmount as string),
-            to_address: Address.parse(env.ton.router),
-            response_address: Address.parse(senderAddress as string),
-            custom_payload: beginCell().storeDict(Dictionary.empty()).endCell(),
-            forward_ton_amount: toNano(2.0),
-            either_payload: true,
-            swap: {
-              kind: "SwapParams",
-              forward_opcode: PoolWrapper.Opcodes.Swap,
-              fee: pool.fee,
-              jetton1_wallet: zeroForOne
-                ? Address.parse(pool.jetton1WalletAddress)
-                : Address.parse(pool.jetton0WalletAddress),
-              sqrt_price_limit: MIN_SQRT_RATIO,
-              tick_spacing: pool.tickSpacing,
-              zero_for_one: zeroForOne,
-            },
-          },
-          {
-            value: toNano(2.5),
-          }
-        );
-        const [afterSenderExecute, afterJettonReceived] = await Promise.all([
-          (async () => {
-            return (
-              await blockchain.getContract(
-                Address.parse(senderAddress as string)
-              )
-            ).balance;
-          })(),
-          (async () => {
-            return (await jettonWalletOutContract!.getBalance()).amount;
-          })(),
-        ]);
-        printTransactionFees(swapTx.transactions);
+      //   let swapTx = await jettonWalletInContract!.sendTransferSwap(
+      //     sender,
+      //     {
+      //       kind: "OpJettonTransferSwap",
+      //       query_id: 0,
+      //       jetton_amount: BigInt(jettonInAmount as string),
+      //       to_address: Address.parse(env.ton.router),
+      //       response_address: Address.parse(senderAddress as string),
+      //       custom_payload: beginCell().storeDict(Dictionary.empty()).endCell(),
+      //       forward_ton_amount: toNano(2.0),
+      //       either_payload: true,
+      //       swap: {
+      //         kind: "SwapParams",
+      //         forward_opcode: PoolWrapper.Opcodes.Swap,
+      //         fee: pool.fee,
+      //         jetton1_wallet: zeroForOne
+      //           ? Address.parse(pool.jetton1WalletAddress)
+      //           : Address.parse(pool.jetton0WalletAddress),
+      //         sqrt_price_limit: MIN_SQRT_RATIO,
+      //         tick_spacing: pool.tickSpacing,
+      //         zero_for_one: zeroForOne,
+      //       },
+      //     },
+      //     {
+      //       value: toNano(2.5),
+      //     }
+      //   );
+      //   const [afterSenderExecute, afterJettonReceived] = await Promise.all([
+      //     (async () => {
+      //       return (
+      //         await blockchain.getContract(
+      //           Address.parse(senderAddress as string)
+      //         )
+      //       ).balance;
+      //     })(),
+      //     (async () => {
+      //       return (await jettonWalletOutContract!.getBalance()).amount;
+      //     })(),
+      //   ]);
+      //   printTransactionFees(swapTx.transactions);
 
-        if (returnedAmount < afterJettonReceived - beforeJettonReceived) {
-          returnedAmount = afterJettonReceived - beforeJettonReceived;
-          data = {
-            receivedAmount: (
-              afterJettonReceived - beforeJettonReceived
-            ).toString(),
-            executeGasConsumed: (
-              afterSenderExecute - beforeSenderExecute
-            ).toString(),
-            zeroForOne,
-            pool,
-          };
-        }
+      // if (returnedAmount < afterJettonReceived - beforeJettonReceived) {
+      // returnedAmount = afterJettonReceived - beforeJettonReceived;
+      data = {
+        receivedAmount: "0",
+        executeGasConsumed: "0".toString(),
+        zeroForOne,
+        pool,
+      };
+      // }
 
-        if (!data) {
-          throw new Error("There amount is too large for swapping");
-        }
-      }
+      // if (!data) {
+      //   throw new Error("There amount is too large for swapping");
+      // }
+      // }
 
       res.status(StatusCodes.OK).json({
         message: "Simulate swap successfully",
